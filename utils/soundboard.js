d29e7fb4c664e8a5ef795f8511da63c76ba3a84b
AndroidDoctorr <gbartthenerd@gmail.com>
2023-05-07 00:17:31 -0400

    added soundboard assets

---
+++ utils/soundboard.js
@@ -0,0 +1,107 @@
+export default {
+  "badumtss": {
+    image: require('../assets/soundboard/badumtss.png'),
+  },
+  "dingding": {
+    image: require('../assets/soundboard/dingding.png'),
+  },
+  "buzzer": {
+    image: require('../assets/soundboard/buzzer.png'),
+  },
+  "klaxon": {
+    image: require('../assets/soundboard/klaxon.png'),
+  },
+  "wahwah": {
+    image: require('../assets/soundboard/wahwah.png'),
+  },
+  "cheering": {
+    image: require('../assets/soundboard/cheering.png'),
+  },
+  "booing": {
+    image: require('../assets/soundboard/booing.png'),
+  },
+  "crickets": {
+    image: require('../assets/soundboard/crickets.png'),
+  },
+  "wrong": {
+    image: require('../assets/soundboard/wrong.png'),
+  },
+  "pwned": {
+    image: require('../assets/soundboard/pwned.png'),
+  },
+  "bttf": {
+    image: require('../assets/soundboard/bttf.png'),
+  },
+  "xfiles": {
+    image: require('../assets/soundboard/xfiles.png'),
+  },
+  "dowhatnow": {
+    image: require('../assets/soundboard/dowhatnow.png'),
+  },
+  "getinthecar": {
+    image: require('../assets/soundboard/getinthecar.png'),
+  },
+  "oohhestryin": {
+    image: require('../assets/soundboard/oohhestryin.png'),
+  },
+  "johncena": {
+    image: require('../assets/soundboard/johncena.png'),
+  },
+  "ostrich": {
+    image: require('../assets/soundboard/ostrich.png'),
+  },
+  "thatswhatshesaid": {
+    image: require('../assets/soundboard/thatswhatshesaid.png'),
+  },
+  "opinion": {
+    image: require('../assets/soundboard/opinion.png'),
+  },
+  "bwahh": {
+    image: require('../assets/soundboard/bwahh.png'),
+  },
+  "shoppin": {
+    image: require('../assets/soundboard/shoppin.png'),
+  },
+  "phrasing": {
+    image: require('../assets/soundboard/phrasing.png'),
+  },
+  "curb": {
+    image: require('../assets/soundboard/curb.png'),
+  },
+  "triggered": {
+    image: require('../assets/soundboard/triggered.png'),
+  },
+  "wilhelm": {
+    image: require('../assets/soundboard/wilhelm.png'),
+  },
+  "bazinga": {
+    image: require('../assets/soundboard/bazinga.png'),
+  },
+  "bruh": {
+    image: require('../assets/soundboard/bruh.png'),
+  },
+  "toast": {
+    image: require('../assets/soundboard/toast.png'),
+  },
+  "myman": {
+    image: require('../assets/soundboard/myman.png'),
+  },
+  "mark": {
+    image: require('../assets/soundboard/mark.png'),
+  },
+  "oof": {
+    image: require('../assets/soundboard/oof.png'),
+  },
+  "pottery": {
+    image: require('../assets/soundboard/pottery.png'),
+  },
+  "thatsthejoke": {
+    image: require('../assets/soundboard/thatsthejoke.png'),
+  },
+  "realestate": {
+    image: require('../assets/soundboard/realestate.png'),
+  },
+  "wtf": {
+    image: require('../assets/soundboard/wtf.png'),
+  },
+}

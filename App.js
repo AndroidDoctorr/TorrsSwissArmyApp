41b90049588cfe0828445445ac03c5dc126361ee
AndroidDoctorr <gbartthenerd@gmail.com>
2023-06-01 16:16:00 -0400

    moved styles to separate file

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
+import styles from './styles'
 import Notepad from './routes/Notepad'
 import Soundboard from './routes/Soundboard'
 
@@ -30,27 +31,3 @@ export default function App() {
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  rouge: {
-    backgroundColor: '#902240',
-  },
-  title: {
-    marginTop: 20,
-    marginBottom: 20,
-    fontSize: 24,
-    fontFamily: 'Roboto',
-    color: '#ffd',
-  },
-  center: {
-    alignSelf: 'center',
-  },
-  fullHeight: {
-    height: '100%',
-  },
-  logo: {
-    height: 100,
-    width: 100,
-    resizeMode: 'contain',
-  }
-})
---
+++ styles/index.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    rouge: {
+        backgroundColor: '#902240',
+    },
+    title: {
+        marginTop: 20,
+        marginBottom: 20,
+        fontSize: 24,
+        fontFamily: 'Roboto',
+        color: '#ffd',
+    },
+    center: {
+        alignSelf: 'center',
+    },
+    fullHeight: {
+        height: '100%',
+    },
+    logo: {
+        height: 100,
+        width: 100,
+        resizeMode: 'contain',
+    }
+})
